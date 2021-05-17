efb469e5a645bf51b702bfc2cb28bdd779e16341
Deva271215 <devaputra78@gmail.com>
2021-05-18 01:52:58 +0800

    this is Hospital App

---
+++ app/src/main/java/com/g_one_hospitalapp/MedRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.g_one_hospitalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_med_record)
+    }
+}
