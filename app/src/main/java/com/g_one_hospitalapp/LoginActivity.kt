5a4d7d8d29e97bec41120db198afae5f3796365a
Deva271215 <devaputra78@gmail.com>
2021-05-26 15:25:24 +0800

    Redirect from login to signup

--- app/src/main/java/com/g_one_hospitalapp/LoginActivity.kt
+++ app/src/main/java/com/g_one_hospitalapp/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.g_one_hospitalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.g_one_hospitalapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onBtnToSignUpClicked()
+    }
+
+    private fun onBtnToSignUpClicked() {
+        binding.btnToSignUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
