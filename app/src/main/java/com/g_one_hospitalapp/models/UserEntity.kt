27b0c957869243b9344d2867cdee734ceb0874f9
Deva271215 <devaputra78@gmail.com>
2021-05-26 15:18:20 +0800

    Added user entity

---
+++ app/src/main/java/com/g_one_hospitalapp/models/UserEntity.kt
@@ -0,0 +1,8 @@
+package com.g_one_hospitalapp.models
+
+data class UserEntity (
+    val group_name: String? = null,
+    val email: String? = null,
+    val telp: String? = null,
+    val password: String? = null,
+)
