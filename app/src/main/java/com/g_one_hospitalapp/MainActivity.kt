42a56ee2416c267630455f35e393f8b005239ee4
Deva271215 <devaputra78@gmail.com>
2021-05-29 22:14:20 +0800

    Redirect from main to chatfield activity and connect to socket with chat id

--- app/src/main/java/com/g_one_hospitalapp/MainActivity.kt
+++ app/src/main/java/com/g_one_hospitalapp/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.g_one_hospitalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.g_one_hospitalapp.databinding.ActivityMainBinding
 import com.g_one_hospitalapp.utilities.UserPreference
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private lateinit var preference: UserPreference
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         preference = UserPreference(applicationContext)
 
         val data = preference.getLoginData()
         FirebaseMessaging.getInstance().subscribeToTopic(data.user?.hospital?.code.toString())
+
+        onOpenChatButtonClicked()
+    }
+
+    fun onOpenChatButtonClicked() {
+        binding.openChatButton.setOnClickListener {
+            val intent = Intent(this, PatientHistoryActivity::class.java)
+            intent.putExtra(PatientHistoryActivity.CHAT_ID, "c1679c13-1a05-4384-b996-b6eafcab575a")
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/g_one_hospitalapp/PatientHistoryActivity.kt
+++ app/src/main/java/com/g_one_hospitalapp/PatientHistoryActivity.kt
@@ -2,10 +2,21 @@ package com.g_one_hospitalapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.g_one_hospitalapp.utilities.SocketIOInstance
 
 class PatientHistoryActivity : AppCompatActivity() {
+    companion object {
+        const val CHAT_ID = "chat_id"
+    }
+
+    private var socket: SocketIOInstance = SocketIOInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_history)
+
+        socket.connectToSocketServer()
+        socket.getSocket()?.connect()
+        socket.getSocket()?.emit("join_chat", CHAT_ID)
     }
 }
