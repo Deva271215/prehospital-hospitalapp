ba5c52119ca08b3c3be8fd88c4fc5a9fd0d6b06b
Deva271215 <devaputra78@gmail.com>
2021-05-29 16:02:04 +0800

    Configure and show notification from Firebase Cloud Messaging

--- app/src/main/java/com/g_one_hospitalapp/utilities/FCMService.kt
+++ app/src/main/java/com/g_one_hospitalapp/utilities/FCMService.kt
@@ -1,6 +1,45 @@
 package com.g_one_hospitalapp.utilities
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.g_one_hospitalapp.MainActivity
+import com.g_one_hospitalapp.R
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class FCMService: FirebaseMessagingService() {}
+class FCMService: FirebaseMessagingService() {
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Log.i("token", p0)
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        Log.i("remote_message_data", p0.data.toString())
+        if (p0.notification != null) {
+            showNotification(p0.notification?.title.toString(), p0.notification?.body.toString())
+        }
+    }
+
+    private fun showNotification(title: String, body: String) {
+        val intent = Intent(this@FCMService, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this@FCMService, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val builder = NotificationCompat.Builder(this@FCMService, "default")
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(0, builder.build())
+        }
+    }
+}
